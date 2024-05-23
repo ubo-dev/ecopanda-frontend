09342078be5931ed19ceb386922235da77279713
Umut Bilal Okur <okur.umutbilal@gmail.com>
2024-05-23 14:25:54 +0300

    navbar seperated and logo added

---
+++ src/pages/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router";
+import "../../index.css";
+
+function Navbar() {
+  const navigate = useNavigate();
+  function handleHome() {
+    navigate("/");
+  }
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <img
+              onClick={handleHome}
+              style={{ width: "140px", "cursor":"pointer" }}
+              src={require("./PNG.png")}
+              alt="ecopanda"
+            ></img>
+            <ul>
+              <li>
+                <a href="#">ABOUT US</a>
+              </li>
+              <li>
+                <a href="#">VISION</a>
+              </li>
+              <li>
+                <a href="#">MISSION</a>
+              </li>
+            </ul>
+            <a href="#">CONTACT US</a>
+          </div>
+        </nav>
+      </header>
+      ;
+    </>
+  );
+}
+export default Navbar;
